ecd877c55862954537f1e4e41f6240fa37f88855
Wai Ting Cheung <waitingcheung@users.noreply.github.com>
2018-06-14 18:24:48 +0800

    Send email when tickets are available
    Add dependencies

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const phantom = require('phantom');
 const cheerio = require('cheerio');
-const NotificationCenter = require('node-notifier').NotificationCenter;
+const nodemailer = require('nodemailer');
 
 const url = 'https://uacinemas.com.hk/eng/movies';
 const prefix = 'https://uacinemas.com.hk/eng/';
-const keyword = 'Ant-Man';
-const notifier = new NotificationCenter();
+
+if (process.env.KEYWORD === undefined) {
+    require('dotenv').load();
+}
+
+const config = {
+    keyword: process.env.KEYWORD,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+    receivers: process.env.RECEIVERS
+};
 
 checkTickets();
 setInterval(checkTickets, 300000);
@@ -28,10 +37,10 @@ function checkTickets() {
                 const movie = movies[k];
                 if (movie.children && movie.children[0]) {
                     const title = movie.children[0].data;
-                    if (title.includes(keyword)) {
+                    if (title.includes(config.keyword)) {
                         console.log(getTimeNow() + ' ' + title);
                         const movieURL = prefix + movie.attribs.href;
-                        checkMovieTickets(movieURL)
+                        checkMovieTickets(title, movieURL)
                     }
                 }
             }
@@ -41,7 +50,7 @@ function checkTickets() {
     })();
 }
 
-function checkMovieTickets(url) {
+function checkMovieTickets(title, url) {
     (async function () {
         const instance = await phantom.create();
         const page = await instance.createPage();
@@ -54,22 +63,42 @@ function checkMovieTickets(url) {
         const $ = cheerio.load(content);
         if ($('div').hasClass('ticketing_box') && $('.ticketing_box').html().includes('BEA IMAX @ UA iSQUARE')) {
             console.log(getTimeNow() + ' Tickets available.');
-            notifier.notify(
-                {
-                    'message': 'Tickets Available',
-                    'open': url,
-                    'wait': true
-                },
-                function (error, response, metadata) {
-                    console.log(response, metadata);
-                }
-            );
+            sendMail(title, url);
         } else {
             console.log(getTimeNow() + ' Tickets not available.');
         }
 
         await instance.exit();
     })();
+}
+
+function sendMail(title, url) {
+    nodemailer.createTestAccount((err, account) => {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: config.user, // generated ethereal user
+                pass: config.pass // generated ethereal password
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Ticketing Bot" <' + config.user + '@gmail.com>', // sender address
+            to: config.receivers, // list of receivers
+            subject: title + ' Tickets Available', // Subject line
+            text: url, // plain text body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+        });
+    })
 }
 
 function getTimeNow() {
