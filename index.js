535d8f5556d3f4e44139e8aaaf94485d1e62aefd
Wai Ting Cheung <waitingcheung@users.noreply.github.com>
2018-06-14 18:38:16 +0800

    Bind app to port

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const phantom = require('phantom');
 const cheerio = require('cheerio');
 const nodemailer = require('nodemailer');
+const express = require('express');
+
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
 
 const url = 'https://uacinemas.com.hk/eng/movies';
 const prefix = 'https://uacinemas.com.hk/eng/';
